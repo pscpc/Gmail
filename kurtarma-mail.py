4d40ea7f9c55de65548cdc9732a3c70e1ff8f790
Ibrahim <ibrahim.ozay97@gmail.com>
2022-07-20 21:12:48 +0300

    pscpc

--- kurtarma-mail.py
+++ kurtarma-mail.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from imap_tools import MailBox
 from imap_tools import AND
+from selenium.webdriver.common.keys import Keys
 
 options = uc.ChromeOptions()
 options.add_argument("--incognito")
